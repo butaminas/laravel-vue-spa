3110e0d22a6c63b2d5a83cdcff3f36b60214d37b
butaminas <mindaugas545@yahoo.com>
2020-02-19 22:32:13 +0200

    Update SetLocale middleware
    Using HTTP_ACCEPT_LANGUAGE in some browsers (firefox) will result in wrong locale being loaded.

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -15,31 +15,31 @@ class SetLocale
      */
     public function handle($request, Closure $next)
     {
-        if ($locale = $this->parseLocale($request)) {
-            app()->setLocale($locale);
+        // read the language from the request header
+        $locale = $request->header('Content-Language');
+
+        // if the header is missed
+        if(!$locale){
+            // take the default local language
+            $locale = app()->config->get('app.locale');
         }
 
-        return $next($request);
-    }
-
-    /**
-     * @param  \Illuminate\Http\Request $request
-     * @return string|null
-     */
-    protected function parseLocale($request)
-    {
-        $locales = config('app.locales');
-
-        $locale = $request->server('HTTP_ACCEPT_LANGUAGE');
-        $locale = substr($locale, 0, strpos($locale, ',') ?: strlen($locale));
-
-        if (array_key_exists($locale, $locales)) {
-            return $locale;
+        // check the languages defined is supported
+        if (!array_key_exists($locale, app()->config->get('app.locales'))) {
+            // respond with error
+            return abort(403, 'Language not supported.');
         }
 
-        $locale = substr($locale, 0, 2);
-        if (array_key_exists($locale, $locales)) {
-            return $locale;
-        }
+        // set the local language
+        app()->setLocale($locale);
+
+        // get the response after the request is done
+        $response = $next($request);
+
+        // set Content Languages header in the response
+        $response->headers->set('Content-Language', $locale);
+
+        // return the response
+        return $response;
     }
 }
